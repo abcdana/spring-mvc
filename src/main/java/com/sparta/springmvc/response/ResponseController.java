6d764cf9f721574be725eada7f475dd10085e368
Daeun Kim <74163296+abcdana@users.noreply.github.com>
2024-11-03 20:30:45 +0900

    Feat : 정적페이지와 동적페이지

---
+++ src/main/java/com/sparta/springmvc/response/ResponseController.java
@@ -0,0 +1,21 @@
+package com.sparta.springmvc.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/response")
+public class ResponseController {
+    // [Response header]
+    //   Content-Type: text/html
+    // [Response body]
+    //   {"name":"Robbie","age":95}
+    @GetMapping("/json/string")
+    @ResponseBody
+    public String helloStringJson() {
+        return "{\"name\":\"Robbie\",\"age\":95}";
+    }
+
+}
